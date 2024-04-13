5736dbce27929e48d74f2f062b309e1f9c116e94
Eric Hu <huericnan@gmail.com>
2024-04-13 10:11:51 -0400

    update

--- mvc/index.js
+++ mvc/index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import userController from "./controllers/userController.js";
 
 const app = express();
 const port = 3001;
 
 app.use(express.json()); // MIddleware that allows Express to parse request body in JSON
+app.use(express.urlencoded({ extended: true })); // Middleware that allows Express to parse request body in FormData
 
 app.set("views", "./views"); // Defines the location of the view templates
 app.set("view engine", "pug"); // Defines the template engine
@@ -14,6 +16,16 @@ app.get("/", (req, res) => {
 });
 
 // The user routes
+app
+  .route("/users")
+  .get(userController.getAllUsers) // Omitted in this lesson
+  .post(userController.createNewUser);
+
+app
+  .route("/users/:id")
+  .get(userController.getUserById) // Omitted in this lesson
+  .put(userController.updateUser)
+  .delete(userController.deleteUser);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}. Visit http://localhost:${port}.`);
