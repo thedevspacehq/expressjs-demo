d2ceb5c995a806eaeb4785ddad40433c76781dd5
Eric Hu <huericnan@gmail.com>
2024-04-13 18:56:48 -0400

    update

--- mvc/controllers/userController.js
+++ mvc/controllers/userController.js
@@ -9,6 +9,16 @@ const userController = {
       });
     });
   },
+  getAllUsers: async function (req, res) {},
+  createNewUser: async function (req, res) {
+    const { username, email } = req.body;
+    console.log(req.body);
+    User.create(username, email, (err, userID) => {
+      res.redirect(`/users/${userID}`);
+    });
+  },
+  updateUser: async function (req, res) {},
+  deleteUser: async function (req, res) {},
 };
 
 export default userController;
